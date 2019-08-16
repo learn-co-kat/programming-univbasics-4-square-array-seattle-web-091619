4ca9fe9b1dfc5195148ef9ec4f1833c97198896b
Katrina R <katrina.reinsdorff@gmail.com>
2019-08-16 18:38:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,11 +5,8 @@ counter = 0
 newarray = []
 
 while array[counter] do
-
-newarray << array[counter]*array[counter] 
-
-counter += 1 
-
+  newarray << array[counter]*array[counter] 
+  counter += 1 
 end 
 
 return newarray
