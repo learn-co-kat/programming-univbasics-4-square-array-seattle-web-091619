63a0d20a938997160f81a4fbb0f823ea22bd4f4d
Katrina R <katrina.reinsdorff@gmail.com>
2019-08-16 18:22:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,16 @@
 def square_array(array)
-  counter = 0 
-    
-  newarray = []
-    
-  while counter <= array.length do 
-    newarray << (array)**2
-    counter += 1 
- end 
+
+newarray = []
+
  
- return newarray
+ 
+return newarray
  
 end
+
+  #counter = 0 
+    
+  #while counter <= array.length do 
+    #newarray << (array)**2
+    #counter += 1 
+ #end 
